199748787715881531185afff79b1325fddacf19
jfranciscohdez <72480603+jfranciscohdez@users.noreply.github.com>
2020-10-16 20:11:48 -0500

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 23:20:09 2020
+
+@author: jfran
+"""
+
+imagen = "Esta Prueba String"
+
+for i in imagen:
+    
+    print("letra: "+ i)
+    
+print("Adios")
